42cf688d4151ec1340374b8f23579cc24ca67320
noplur <geoduckphoto@gmail.com>
2021-04-27 22:53:19 -0700

    added client side

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { authMiddleware } = require('./utils/auth');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -28,4 +29,13 @@ db.once('open', () => {
     // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
+});
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
