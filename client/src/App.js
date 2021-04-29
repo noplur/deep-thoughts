8b01a03d68adcabdeeca1add49c5da8358befc05
noplur <geoduckphoto@gmail.com>
2021-04-28 23:23:52 -0700

    added routes feature

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,13 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 const client = new ApolloClient({
   uri: '/graphql'
 });
@@ -16,13 +23,25 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Router>
       <div className="flex-column justify-flex-start min-100-vh">
         <Header />
         <div className="container">
-          <Home />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/thought" component={SingleThought} />
+            <Route exact path="/profile/:username?" component={Profile} />
+            <Route exact path="/thought/:id" component={SingleThought} />
+            <Route component={NoMatch} />
+          </Switch>
         </div>
+
         <Footer />
       </div>
+      </Router>
     </ApolloProvider>
   );
 }
