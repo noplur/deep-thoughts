f51388facaece4fdd862f58b41c33e8196b53028
noplur <geoduckphoto@gmail.com>
2021-04-29 21:20:19 -0700

    added forms

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,7 @@ import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
 import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Home = () => {
   // use useQuery hook to make query request
@@ -16,23 +17,28 @@ const Home = () => {
 
   return (
     <main>
-      <div className="flex-row justify-space-between">
-        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
-            {loading ? (
-              <div>Loading...</div>
-            ) : (
-              <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
-            )}
+        <div className="flex-row justify-space-between">
+          {loggedIn && (
+            <div className="col-12 mb-3">
+              <ThoughtForm />
+            </div>
+          )}
+          <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+          )}
         </div>
-              {loggedIn && userData ? (
+        {loggedIn && userData ? (
           <div className="col-12 col-lg-3 mb-3">
-          <FriendList
-            username={userData.me.username}
-            friendCount={userData.me.friendCount}
-            friends={userData.me.friends}
-          />
-  </div>
-) : null}
+            <FriendList
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friends={userData.me.friends}
+            />
+          </div>
+        ) : null}
       </div>
     </main>
   );
